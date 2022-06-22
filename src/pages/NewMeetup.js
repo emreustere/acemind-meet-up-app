6880331d6467baf025c5c8f22f014b4e10e9f360
rippy56 <ustereemre@gmail.com>
2022-06-23 01:41:11 +0300

    App complete

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+	const navigate = useNavigate();
+
+	function addMeetupHandler(meetupData) {
+		fetch(
+			"https://acemind-meet-up-app-default-rtdb.europe-west1.firebasedatabase.app//meetups.json",
+
+			{
+				method: "POST",
+				body: JSON.stringify(meetupData),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		).then(() => {
+			navigate("/", { replace: true });
+		});
+	}
+
+	return (
+		<section>
+			<h1>Add New Meetup</h1>
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
+		</section>
+	);
+}
+
+export default NewMeetupPage;
